0474bbc32478657702fa303a5fb9d015ee577fc7
Tyler Beringer <99tylerberinger@gmail.com>
2021-01-08 22:31:26 -0500

    Removed options hardcoding, renamed some variables

--- src/main.py
+++ src/main.py
@@ -1,30 +1,37 @@
 import csv
 
 
-print("----")
-print("my-albums-ordered")
-print("OPTIONS: album, artist, date")
-print("Type which option you want and press enter")
-sort_setting = input()
-print("----")
+with open("albums.csv", "r") as albumsfile:
 
-# Select albums.csv
-with open("albums.csv", "r") as csvfile:
+	albumsfile_read = csv.DictReader(albumsfile, delimiter=";")
 
-	# Read albums.csv content
-	csvfile_read = csv.DictReader(csvfile, delimiter=";")
+	print("----")
+	print("my-albums-ordered")
+	print("OPTIONS:")
+	options = albumsfile_read.fieldnames
+	for i in range(len(options)):
+		print(f"{i}. '{options[i]}'")
+	print("Which one do you want to sort by?")
+	# Get decision
+	while True:
+		# make sure it's a number
+		try:
+			decision = int(input("Enter number: "))
+		except ValueError:
+			continue
+		# make sure it's valid
+		if decision >= 0 and decision < len(options):
+			break
+	sort_setting = options[decision]
+	print(f"You selected '{sort_setting}'")
 
-	# Sort content by sort_setting
-	sorted_albums = sorted(csvfile_read, key=lambda row : row[sort_setting])
-	print("Sorted the albums by " + sort_setting)
+	sorted_albums = sorted(albumsfile_read, key=lambda row : row[sort_setting])
+	print(f"Sorted the albums by {sort_setting}")
 
-	# Write sorted content to sortedalbums.csv
-	with open("sortedalbums.csv", "w") as csvfile:
-		csvfile_write = csv.DictWriter(csvfile, delimiter=";", fieldnames=["album", "artist", "date"])
-		csvfile_write.writeheader()
+	with open("albums.csv", "w") as albumsfile:
+		albumsfile_write = csv.DictWriter(albumsfile, delimiter=";", fieldnames=options)
+		albumsfile_write.writeheader()
 		for row in sorted_albums:
-			csvfile_write.writerow(row)
-		print("Printed the sorted albums to sortedalbums.csv")
+			albumsfile_write.writerow(row)
+		print(f"Printed the albums by {sort_setting}")
 
-
-print()
